1ceaae1dc978987d437992fd11bce14e051a5fdd
poojamande0807 <mandeakshaykumar@gmail.com>
2024-09-21 23:56:29 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h1>Please log in</h1>
+
+      {/* You can implement a login form here */}
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Users.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Users() {
+
+  return (
+
+    <div>
+
+      <h1>List of Users</h1>
+
+      {/* You can display user-related information here */}
+
+    </div>
+
+  );
+
+}
+
+export default Users;
